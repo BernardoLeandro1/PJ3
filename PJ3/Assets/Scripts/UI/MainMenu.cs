afbe9dd326af40c61bada205d8425175b3ef76f2
Daniel <41786@ufp.edu.pt>
2024-07-08 04:38:11 +0100

    MainMenu done; Options Menu almost done
    Only controls key mapping missing on options menu

--- PJ3/Assets/Scripts/UI/MainMenu.cs
+++ PJ3/Assets/Scripts/UI/MainMenu.cs
@@ -5,20 +5,8 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public GameObject ExitPanel;
-    public GameObject Options;
-    public GameObject Main;
-
     public void PlayGame (){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void ShowQuitText (){
-        ExitPanel.SetActive(true);
-    }
-
-    public void HideQuitText (){
-        ExitPanel.SetActive(false);
     }
 
     public void QuitGame (){
